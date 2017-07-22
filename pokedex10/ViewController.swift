5a865b681855274d97f005bad63a527997075827
LALIT JAGTAP <jagtap_lalit@yahoo.com>
2017-07-22 14:23:36 -0500

    Initial Commit

---
+++ pokedex10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex10
+//
+//  Created by LALIT JAGTAP on 7/22/17.
+//  Copyright © 2017 LALIT JAGTAP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
